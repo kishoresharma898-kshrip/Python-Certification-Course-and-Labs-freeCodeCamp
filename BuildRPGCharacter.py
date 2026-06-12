3e92c9ed191af9580a2070f41a2a64cd4bedc085
kishoresharma898-kshrip <kishoresharma898@gmail.com>
2026-06-12 20:52:21 +0530

    Update BuildRPGCharacter.py
    Added numbering to the User Stories.

--- BuildRPGCharacter.py
+++ BuildRPGCharacter.py
@@ -5,19 +5,19 @@ Objective: Fulfill the user stories below and get all the tests to pass to compl
 
 User Stories:
 
-You should have a function named create_character.
-The function should accept, in order, a character name followed by three stats: strength, intelligence, and charisma.
+1. You should have a function named create_character.
+2. The function should accept, in order, a character name followed by three stats: strength, intelligence, and charisma.
 The character name should be validated:
-If the character name is not a string, the function should return The character name should be a string.
-If the character name is an empty string, the function should return The character should have a name.
-If the character name is longer than 10 characters, the function should return The character name is too long.
-If the character name contains spaces, the function should return The character name should not contain spaces.
+3. If the character name is not a string, the function should return The character name should be a string.
+4. If the character name is an empty string, the function should return The character should have a name.
+5. If the character name is longer than 10 characters, the function should return The character name is too long.
+6. If the character name contains spaces, the function should return The character name should not contain spaces.
 The stats should also be validated:
-If one or more stats are not integers, the function should return All stats should be integers.
-If one or more stats are less than 1, the function should return All stats should be no less than 1.
-If one or more stats are more than 4, the function should return All stats should be no more than 4.
-If the sum of all stats is different than 7, the function should return The character should start with 7 points.
-If all values pass the verification, the function should return a string with four lines:
+7. If one or more stats are not integers, the function should return All stats should be integers.
+8. If one or more stats are less than 1, the function should return All stats should be no less than 1.
+9. If one or more stats are more than 4, the function should return All stats should be no more than 4.
+10. If the sum of all stats is different than 7, the function should return The character should start with 7 points.
+11. If all values pass the verification, the function should return a string with four lines:
 the first line should contain the character name
 lines 2-4 should start with the stat abbreviation, STR, INT or CHA (in this order), then a space, and then a number of full dots (●) equal to the value of the stat, and a number of empty dots (○) to reach 10. Example: if the value of strength is 3 there must be 3 full dots followed by 7 empty dots. The dots are given in the editor.
 Here's the string that should be returned by create_character('ren', 4, 2, 1):
